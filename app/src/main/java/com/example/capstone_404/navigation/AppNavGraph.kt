6d98c98d92cb3dd79dc1181891c426c9a181b853
Heo02 <hsy3160@naver.com>
2025-07-22 22:13:22 +0900

    [Design/#12] 공용 Bottom Navigation Bar 구현

--- app/src/main/java/com/example/capstone_404/navigation/AppNavGraph.kt
+++ app/src/main/java/com/example/capstone_404/navigation/AppNavGraph.kt
@@ -1,34 +1,76 @@
 package com.example.capstone_404.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.example.capstone_404.ui.login.view.LoginScreen
-import com.example.capstone_404.ui.login.view.ProfileInputScreen
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.capstone_404.ui.component.BottomNavigationBar
+import com.example.capstone_404.ui.component.bottomTabs
+import com.example.capstone_404.ui.group.GroupScreen
+import com.example.capstone_404.ui.login.LoginScreen
+import com.example.capstone_404.ui.login.ProfileInputScreen
 
+// 페이지 만들 때 추가 해야됨
 @Composable
-fun AppNavGraph(
-    navController: NavHostController
-) {
-    NavHost(
-        navController = navController,
-        startDestination = Route.LOGIN
-    ) {
-        composable(Route.LOGIN) {
-            LoginScreen(
-                onLoginSuccess = {
-                    navController.navigate(Route.PROFILE_INPUT)
-                }
-            )
+fun AppNavGraph(navController: NavHostController) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    Scaffold(
+        bottomBar = {
+            if (currentRoute in bottomTabs.map { it.route }) {
+                BottomNavigationBar(
+                    currentRoute = currentRoute,
+                    onTabSelected = { route ->
+                        navController.navigate(route) {
+                            popUpTo(navController.graph.startDestinationId) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    }
+                )
+            }
         }
-        // 임시 연결 상태
-        composable(Route.PROFILE_INPUT) {
-            ProfileInputScreen(
-                onStartClicked = {
-                    navController.navigate(Route.PROFILE_INPUT)
-                }
-            )
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Route.LOGIN,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            // 로그인
+            composable(Route.LOGIN) {
+                LoginScreen(
+                    onLoginSuccess = {
+                        navController.navigate(Route.PROFILE_INPUT)
+                    }
+                )
+            }
+            // 추가 정보 입력
+            composable(Route.PROFILE_INPUT) {
+                ProfileInputScreen(
+                    onStartClicked = {
+                        navController.navigate(Route.GROUP)
+                    }
+                )
+            }
+            // 그룹 메인
+            composable(Route.GROUP) {
+                GroupScreen(
+                )
+            }
+
+            // 임시 정의
+            composable(Route.CALENDAR) {  }
+            composable(Route.DIARY) {  }
+            composable(Route.ALBUM) {  }
+            composable(Route.MYPAGE) {  }
         }
     }
 }
---
+++ app/src/main/java/com/example/capstone_404/ui/component/BottomNavigationBar.kt
@@ -0,0 +1,50 @@
+package com.example.capstone_404.ui.component
+
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.capstone_404.navigation.Route
+import com.example.capstone_404.ui.theme.Main
+import com.example.capstone_404.ui.theme.Stroke
+
+// 따로 호출 안 해도 됨
+@Composable
+fun BottomNavigationBar(
+    currentRoute: String?,
+    onTabSelected: (String) -> Unit
+) {
+    NavigationBar(containerColor = Color.White) {
+        bottomTabs.forEach { tab ->
+            val selected = tab.route == currentRoute
+            NavigationBarItem(
+                selected = selected,
+                onClick = { if (!selected) onTabSelected(tab.route) },
+                icon = {
+                    Icon(
+                        painter = painterResource(tab.iconResId),
+                        contentDescription = tab.label,
+                    )
+                },
+                colors = NavigationBarItemDefaults.colors(
+                    selectedIconColor = Color.Black,
+                    unselectedIconColor = Stroke,
+                    indicatorColor = Main
+                )
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BottomTapPreview() {
+    BottomNavigationBar(
+        currentRoute = Route.GROUP,
+        onTabSelected = {}
+    )
+}

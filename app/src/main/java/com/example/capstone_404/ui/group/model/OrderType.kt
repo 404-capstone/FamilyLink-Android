61d4d431e4aa38e0b16d75509113e61bd96623c5
Heo02 <hsy3160@naver.com>
2025-07-24 22:05:19 +0900

    [Design/#17] 역할 선택 관련 model 정의

---
+++ app/src/main/java/com/example/capstone_404/ui/group/model/OrderType.kt
@@ -0,0 +1,10 @@
+package com.example.capstone_404.ui.group.model
+
+// 아들&딸 첫째~다섯째
+enum class OrderType(val label: String) {
+    FIRST("첫째"),
+    SECOND("둘째"),
+    THIRD("셋째"),
+    FOURTH("넷째"),
+    FIFTH("다섯째")
+}

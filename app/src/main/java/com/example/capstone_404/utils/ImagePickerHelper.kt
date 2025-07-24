2139dd04cc8c0924d88246a38f4456f815392ca1
Heo02 <hsy3160@naver.com>
2025-07-24 15:11:00 +0900

    [Design/#15] 사진 선택 옵션 다이얼로그 구현

---
+++ app/src/main/java/com/example/capstone_404/utils/ImagePickerHelper.kt
@@ -0,0 +1,31 @@
+package com.example.capstone_404.utils
+
+import android.content.ContentValues
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.core.content.FileProvider
+import java.io.File
+
+// 촬영한 사진 Uri로 변환
+fun createImageUri(context: Context): Uri? {
+    val contentResolver = context.contentResolver
+    val contentValues = ContentValues().apply {
+        put(MediaStore.Images.Media.DISPLAY_NAME, "group_photo_${System.currentTimeMillis()}.jpg")
+        put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+    }
+
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+    } else {
+        val imagesDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+        val file = File(imagesDir, "group_photo_${System.currentTimeMillis()}.jpg")
+        FileProvider.getUriForFile(
+            context,
+            "${context.packageName}.fileprovider",
+            file
+        )
+    }
+}

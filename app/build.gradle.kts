7dbd1cf140e47d9022374da745f1e56722a0f0c6
Heo02 <hsy3160@naver.com>
2025-07-27 16:19:19 +0900

    [Feat/#21] DataStore 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,8 +77,8 @@ dependencies {
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation ("com.squareup.okhttp3:okhttp:4.9.3")
-    // DataStore 모듈
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    // DataStore
+    implementation(libs.androidx.datastore.preferences)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)

c50c3a9d15765a7026e431cb5fdd8c21cb48e3b6
Heo02 <hsy3160@naver.com>
2025-10-31 17:42:51 +0900

    [Design/#66] Color.kt 수정

--- app/src/main/java/com/example/capstone_404/ui/theme/Color.kt
+++ app/src/main/java/com/example/capstone_404/ui/theme/Color.kt
@@ -1,9 +1,10 @@
 package com.example.capstone_404.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
 // Main Color
-val Main = Color(0xFFA7D9A1)
+val Main = Color(0xFF00897B)
 val Sub = Color(0xFFFFFFFF)
 val Sub2 = Color(0xFFFFE7A0)
 val Error = Color(0xFFE05744)
@@ -16,28 +17,35 @@ val ButtonDisabled = Color(0xFFD9E4D5)
 
 // Background
 val Background = Color(0xFFF5F9F3)
+val Bg1 = Color(0xFFD1FAE5)
+val GradientBg = Brush.linearGradient(
+    colorStops = arrayOf(
+        0.0f to Bg1,
+        0.5f to Color.White,
+        1.0f to Bg1
+    )
+)
 
 // Text
-val TextMain = Color(0xFFA7D9A1)
 val TextBlack = Color(0xFF0A121A)
 val TextWhite = Color(0xFFFFFFFF)
 val TextGray = Color(0xFFD0D1DA)
 
 // Role Color
-val Grandpa = Color(0xFF2C6E6C)
-val grandma = Color(0xFFA3E4D7)
+val Grandpa = Color(0xFFDCE775)
+val grandma = Color(0xFFF48FB1)
 
-val Dad = Color(0xFF3D9970)
-val Mom = Color(0xFFFF6B6B)
+val Dad = Color(0xFF7CB342)
+val Mom = Color(0xFFD81B60)
 
-val FirstSon = Color(0xFF2471A3)
-val SecondSon = Color(0xFF3498DB)
-val ThirdSon = Color(0xFF5DADE2)
-val FourthSon = Color(0xFF85C1E9)
-val FifthSon = Color(0xFFD6EAF8)
+val FirstSon = Color(0xFF0D47A1)
+val SecondSon = Color(0xFF1976D2)
+val ThirdSon = Color(0xFF42A5F5)
+val FourthSon = Color(0xFF90CAF9)
+val FifthSon = Color(0xFFE3F2FD)
 
-val FirstDaughter = Color(0xFF9B59B6)
-val SecondDaughter = Color(0xFFBB8FCE)
-val ThirdDaughter = Color(0xFFD7BDE2)
-val FourthDaughter = Color(0xFFE8DAEF)
-val FifthDaughter = Color(0xFFF4ECF7)
+val FirstDaughter = Color(0xFF4A148C)
+val SecondDaughter = Color(0xFF8E24AA)
+val ThirdDaughter = Color(0xFFAB47BC)
+val FourthDaughter = Color(0xFFE1BEE7)
+val FifthDaughter = Color(0xFFF3E5F5)
